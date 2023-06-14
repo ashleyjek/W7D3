b2a443e75881acdc14215ae7d9a7e6986c9dd43d
ashley <ashleykimca@gmail.com>
2023-06-14 17:49:34 -0400

    added specs for userscontroller index

--- goal-app/spec/requests/users_spec.rb
+++ goal-app/spec/requests/users_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include("All Users")
     end
+
+    it 'lists the username of all users' do
+      user = create(:user)
+      get users_url
+      expect(response.body).to include(user.username)
+    end
+
   end
+
 end
